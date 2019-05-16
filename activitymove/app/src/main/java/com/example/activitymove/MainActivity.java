473e5974f2e157ae3b718f1eddfd2ce3d0962829
seoyl98 <seoyla98@gmail.com>
2019-05-16 16:01:20 +0900

    메인 인텐트완료

--- activitymove/app/src/main/java/com/example/activitymove/MainActivity.java
+++ activitymove/app/src/main/java/com/example/activitymove/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.activitymove;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.content.Intent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends  AppCompatActivity{
 
@@ -24,10 +25,33 @@ public class MainActivity extends  AppCompatActivity{
         listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(adapter);
 
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location),"교내 지도");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.parking_location),"주차장 지도");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location),"음식점 리스트");
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location),"PC방 리스트");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location), "교내 지도");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.parking_location), "주차장 지도");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location), "음식점 리스트");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.map_location), "PC방 리스트");
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                if(position == 0){
+                    Intent intent1 = new Intent(MainActivity.this, SubActivity.class);
+                    startActivity(intent1);
+                }
+                else if(position == 1) {
+                    Intent intent2 = new Intent(MainActivity.this, SubActivity2.class);
+                    startActivity(intent2);
+                }
+                else if(position == 2) {
+                    Intent intent3 = new Intent(MainActivity.this, SubActivity3.class);
+                    startActivity(intent3);
+                }
+                else if(position == 3){
+                    Intent intent4 = new Intent(MainActivity.this, SubActivity4.class);
+                    startActivity(intent4);
+                }
+
+            }
+        });
     }
 }
 
