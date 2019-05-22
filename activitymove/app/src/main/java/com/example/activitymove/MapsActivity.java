28a9ff0f7638bbac964abb2da7f33a89ace69cbf
seoyl98 <seoyla98@gmail.com>
2019-05-22 17:01:32 +0900

    공대 마크 설정추가

--- activitymove/app/src/main/java/com/example/activitymove/MapsActivity.java
+++ activitymove/app/src/main/java/com/example/activitymove/MapsActivity.java
@@ -7,6 +7,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +39,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Engineering4  = new LatLng(33.454724, 126.565135);
+        mMap.addMarker(new MarkerOptions().position(Engineering4 ).title("공과대학 4호관"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Engineering4 ));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Engineering4 ,17));
+
+
+
     }
+
 }
