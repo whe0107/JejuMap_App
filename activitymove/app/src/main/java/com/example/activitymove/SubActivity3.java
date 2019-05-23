865c14d10751bbff512fcfe1dd2adb8c60ce34d8
seoyl98 <seoyla98@gmail.com>
2019-05-23 15:21:51 +0900

    양식 버튼설정

--- activitymove/app/src/main/java/com/example/activitymove/SubActivity3.java
+++ activitymove/app/src/main/java/com/example/activitymove/SubActivity3.java
@@ -2,16 +2,22 @@ package com.example.activitymove;
 
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
+import android.content.Intent;
 
 public class SubActivity3 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub3);
+    }
+    public void onClick(View view) {
+        if (view.getId() == R.id.w){
+            Intent intent1 = new Intent(this,food_western.class);
+            startActivity(intent1);
+        }
 
     }
 }
