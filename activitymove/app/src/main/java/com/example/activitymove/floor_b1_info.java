07a0e865493e9f4a8d3977bfec2cc8a4a71b09c1
whe0107 <whe0107@naver.com>
2019-05-26 15:45:42 +0900

    층 정보창 자바파일&layout

--- activitymove/app/src/main/java/com/example/activitymove/floor_b1_info.java
+++ activitymove/app/src/main/java/com/example/activitymove/floor_b1_info.java
@@ -4,12 +4,12 @@ package com.example.activitymove;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-public class pcinfo2 extends AppCompatActivity {
+public class floor_b1_info extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pcinfo2);
+        setContentView(R.layout.floor_b1);
 
     }
 }
