a63ad7bcaef2b8ba6864cfbd0c09d0627e883bd7
whe0107 <whe0107@naver.com>
2019-05-28 21:51:39 +0900

    주차장 정보 수정, 건물층지도 이미지 업뎃

---
+++ activitymove/app/src/main/java/com/example/activitymove/floor_3_info.java
@@ -0,0 +1,15 @@
+package com.example.activitymove;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class floor_3_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.floor_3);
+
+    }
+}
